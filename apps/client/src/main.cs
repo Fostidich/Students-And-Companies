ab54608fa4cdda094f2c2426ae7d43b06cd7a746
Fostidich <francesco.ostidich@gmail.com>
2024-10-12 18:11:17 +0200

    documents folders and DD toc

---
+++ apps/client/src/main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("This is the client!");
+        Console.Write("Enter your name: ");
+        string input = Console.ReadLine();
+        Console.WriteLine("Hi, " + input + "!");
+    }
+}
