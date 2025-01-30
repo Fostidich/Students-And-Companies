f7132b34bf5f09ece3a7136cf676047784894d48
Fostidich <francesco.ostidich@gmail.com>
2025-01-30 14:45:34 +0100

    vite config merge

--- web-server/vite.config.js
+++ web-server/vite.config.js
@@ -1,18 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig(({ mode }) => {
-  if (mode === 'development')
-    return {
-      plugins: [react()],
-      define: {
-        'window.env': {
-          VITE_API_SERVER_URL: process.env.VITE_API_SERVER_URL || 'http://localhost:5000',
+export default defineConfig(({ mode }) => ({
+  base: "/",
+  plugins: [react()],
+  define: mode === "development"
+    ? {
+        "window.env": {
+          VITE_API_SERVER_URL: process.env.VITE_API_SERVER_URL || "http://localhost:5000",
         },
-      },
-    };
-  else
-    return {
-      plugins: [react()],
-    };
-});
+      }
+    : {},
+}));
